368e09c943738cd1aa1a66e89fd9c4aa26822edf
mahmudulislammoon <moon.m360ict@gmail.com>
2025-05-08 11:10:35 +0600

    d

--- dist/utils/miscellaneous/schema.js
+++ dist/utils/miscellaneous/schema.js
@@ -5,10 +5,10 @@ class Schema {
         this.PUBLIC_SCHEMA = 'public';
         this.ADMIN_SCHEMA = 'admin';
         this.DBO_SCHEMA = 'dbo';
-        this.B2C_SCHEMA = 'btoc';
+        this.B2C_SCHEMA = 'b2c';
         this.SERVICE_SCHEMA = 'services';
         this.AGENT_SCHEMA = 'agent';
-        this.AGENT_B2C_SCHEMA = 'agent_btoc';
+        this.AGENT_B2C_SCHEMA = 'agent_b2c';
         this.EXTERNAL_SCHEMA = 'external';
     }
 }
--- src/utils/miscellaneous/schema.ts
+++ src/utils/miscellaneous/schema.ts
@@ -2,9 +2,9 @@ export default class Schema {
   protected readonly PUBLIC_SCHEMA = 'public';
   protected readonly ADMIN_SCHEMA = 'admin';
   protected readonly DBO_SCHEMA = 'dbo';
-  protected readonly B2C_SCHEMA = 'btoc';
+  protected readonly B2C_SCHEMA = 'b2c';
   protected readonly SERVICE_SCHEMA = 'services';
   protected readonly AGENT_SCHEMA = 'agent';
-  protected readonly AGENT_B2C_SCHEMA = 'agent_btoc';
+  protected readonly AGENT_B2C_SCHEMA = 'agent_b2c';
   protected readonly EXTERNAL_SCHEMA = 'external';
 }
