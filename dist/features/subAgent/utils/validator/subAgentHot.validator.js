8dd4dbc83ed65b58b984eea07905ec88e62bfc2c
mahmudulislammoon <moon.m360ict@gmail.com>
2025-09-01 18:49:37 +0600

    d

---
+++ dist/features/subAgent/utils/validator/subAgentHot.validator.js
@@ -0,0 +1,128 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const joi_1 = __importDefault(require("joi"));
+class SubAgentHotelValidator {
+    constructor() {
+        this.searchValidator = joi_1.default.object({
+            currency: joi_1.default.string().optional(),
+            client_nationality: joi_1.default.string().required(),
+            destination: joi_1.default.string().valid('City', 'Hotel').required(),
+            code: joi_1.default.number().required(),
+            name: joi_1.default.string().required(),
+            rooms: joi_1.default.array()
+                .items(joi_1.default.object({
+                adults: joi_1.default.number().required(),
+                no_of_infants: joi_1.default.number().optional(),
+                children_ages: joi_1.default.array().items(joi_1.default.number()).optional(),
+            }))
+                .required(),
+            checkin: joi_1.default.string().required(),
+            checkout: joi_1.default.string().required(),
+        });
+        this.getHotelSearchHistoryValidator = joi_1.default.object({
+            from_date: joi_1.default.date().optional(),
+            to_date: joi_1.default.date().optional(),
+            limit: joi_1.default.number().optional(),
+            skip: joi_1.default.number().optional(),
+        });
+        this.getHotelRoomsValidator = joi_1.default.object({
+            hcode: joi_1.default.number().required(),
+            search_id: joi_1.default.string().required(),
+        });
+        this.hotelRoomRecheck = joi_1.default.object({
+            search_id: joi_1.default.string().required(),
+            nights: joi_1.default.number().required(),
+            rooms: joi_1.default.array()
+                .items({
+                rate_key: joi_1.default.string().required(),
+                group_code: joi_1.default.string().required(),
+            })
+                .min(1)
+                .required(),
+        });
+        this.hotelBooking = joi_1.default.object({
+            search_id: joi_1.default.string().required(),
+            hotel_code: joi_1.default.number().required(),
+            group_code: joi_1.default.string().required(),
+            city_code: joi_1.default.number().required(),
+            checkin: joi_1.default.string().required(),
+            checkout: joi_1.default.string().required(),
+            booking_comments: joi_1.default.string().optional(),
+            booking_items: joi_1.default.string()
+                .custom((value, helpers) => {
+                try {
+                    const parsedValue = JSON.parse(value);
+                    const validator = joi_1.default.array()
+                        .items(joi_1.default.object({
+                        room_code: joi_1.default.string().required(),
+                        rate_key: joi_1.default.string().required(),
+                        rooms: joi_1.default.array()
+                            .items(joi_1.default.object({
+                            room_reference: joi_1.default.string().required(),
+                            paxes: joi_1.default.array()
+                                .items(joi_1.default.object({
+                                title: joi_1.default.string()
+                                    .valid('Mr.', 'Ms.', 'Mrs.', 'Mstr.')
+                                    .required(),
+                                name: joi_1.default.string().required(),
+                                surname: joi_1.default.string().required(),
+                                type: joi_1.default.string().valid('AD', 'CH').required(),
+                            }))
+                                .min(1)
+                                .required(),
+                        }))
+                            .min(1)
+                            .required(),
+                    }))
+                        .min(1)
+                        .required();
+                    const { error } = validator.validate(parsedValue);
+                    if (error) {
+                        return helpers.message({ custom: error.message });
+                    }
+                    return parsedValue;
+                }
+                catch (_a) {
+                    return helpers.message({
+                        custom: 'Invalid JSON format in booking_items',
+                    });
+                }
+            })
+                .required(),
+            holder: joi_1.default.string().custom((value, helpers) => {
+                try {
+                    const parsedValue = JSON.parse(value);
+                    const validator = joi_1.default.object({
+                        title: joi_1.default.string().required(),
+                        name: joi_1.default.string().required(),
+                        surname: joi_1.default.string().required(),
+                        email: joi_1.default.string().email().optional(),
+                        phone_number: joi_1.default.string().optional(),
+                        client_nationality: joi_1.default.string().required(),
+                    }).required();
+                    const { error } = validator.validate(parsedValue);
+                    if (error) {
+                        return helpers.message({ custom: error.message });
+                    }
+                    return parsedValue;
+                }
+                catch (_a) {
+                    return helpers.message({
+                        custom: 'Invalid JSON format in holder',
+                    });
+                }
+            }),
+        });
+        this.getHotelBooking = joi_1.default.object({
+            filter: joi_1.default.string().optional(),
+            from_date: joi_1.default.date().optional(),
+            to_date: joi_1.default.date().optional(),
+            limit: joi_1.default.string().optional(),
+            skip: joi_1.default.string().optional(),
+        });
+    }
+}
+exports.default = SubAgentHotelValidator;
